13afa4602c6a3d9a95249d3f08523c30acaa0ee6
Susleen <114741775+Kamoe7@users.noreply.github.com>
2025-07-27 15:09:21 +0545

    Manaul Ack + DLQ support

--- consumer_service/consumer.py
+++ consumer_service/consumer.py
@@ -1,20 +1,35 @@
 import pika
 import json
 
+def process(data):
+    if data["user_id"] == 42:
+        raise Exception('Simulated processing error!')
+
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-#declare exchange and a unique queue
-channel.exchange_declare(exchange='micro_topic',exchange_type='topic',)
-result = channel.queue_declare('',exclusive=True)
-queue_name = result.method.queue
+#declare DLX exchange and queue
+channel.exchange_declare(exchange='dlx',exchange_type='fanout')
+channel.queue_declare(queue='dead_letters')
+channel.queue_bind(exchange='dlx',queue='dead_letters')
 
-channel.queue_bind(exchange='micro_topic',queue=queue_name,routing_key='user.*')
+#declare exchange and a unique queue
+args = {'x-dead-letter-exchange' : 'dlx'}
+channel.exchange_declare(exchange='micro_topic',exchange_type='topic',)
+channel.queue_declare(queue='user_queue',durable=True,arguments=args)
+channel.queue_bind(exchange='micro_topic',queue='user_queue',routing_key='user.*')
 
 def callback(ch,method,properties,body):
-    data = json.loads(body.decode())
-    print(f" [x] Received {data}")
+    try:
+        data = json.loads(body.decode())
+        print(f" [x] Received {data}")
+        process(data)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
-channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-print('[*] Waiting for user related messages...')
+    except Exception as e:
+        print(f"[!] Error: {e} - sending to DLQ")
+        ch.basic_nack(delivery_tag= method.delivery_tag,requeue= False)
+
+channel.basic_consume(queue='user_queue', on_message_callback=callback)
+print('[*] Waiting for retry an DLQ')
 channel.start_consuming()
